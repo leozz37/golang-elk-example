6c3e714b7ea19f4daf5e49cd210ad786d118f425
Leonardo Lima <leonardoaugusto287@gmail.com>
2021-04-22 06:40:07 -0300

    feat: Gin and Zap integration

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/leozz37/golang-elk-example
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.1
+require (
+	github.com/gin-contrib/zap v0.0.1
+	github.com/gin-gonic/gin v1.7.1
+	go.uber.org/zap v1.16.0
+)
--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+	"time"
+
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+const logPath = "./logs/go.log"
+
+var logger *zap.Logger
+
+func setupLog() {
+	os.OpenFile(logPath, os.O_RDONLY|os.O_CREATE, 0666)
+	c := zap.NewProductionConfig()
+	c.OutputPaths = []string{"stdout", logPath}
+	logger, _ = c.Build()
+}
 
 func main() {
+	setupLog()
+
 	r := gin.Default()
+
+	// Setting GIN to use zap as logger
+	r.Use(ginzap.Ginzap(logger, time.RFC3339, true))
+	r.Use(ginzap.RecoveryWithZap(logger, true))
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
